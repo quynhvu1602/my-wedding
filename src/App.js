bb2b9488e62a077edc050c31af007d76df1646f0
Quynh <quynhvungoc1602@gmail.com>
2023-10-05 14:59:41 +0700

    minor fix

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
     window.addEventListener("scroll", (e) => {
       if (!ref?.current) return;
       var sticky = ref?.current?.offsetTop;
-      if (window.scrollY >= sticky) {
+      if (window.scrollY > sticky) {
         ref?.current?.classList.add("sticky");
       } else {
         ref?.current?.classList.remove("sticky");
@@ -46,22 +46,26 @@ function App() {
 
   return (
     <div className="w-full relative bg-white">
-      <section id="home" className="h-screen w-screen bg-blue-300 relative overflow-hidden">
-        <img src={bg} alt="bg" className="hidden lg:block overflow-hidden" />
-        <img src={g7} alt="bg" className="block lg:hidden w-full h-full " />
-        <div className="w-full h-full z-10 absolute top-0 left-0 flex flex-col items-center justify-end text-white space-y-8 p-4 pb-16 md:p-10 lg:p-20 title">
-          <p className="text-[44px] md:text-[64px] text-center">
-            Ngọc Quỳnh <br className="block md:hidden" />&
-            <br className="block md:hidden" /> Việt Hoàn
+      <section
+        id="home"
+        className={`w-screen h-screen lg:h-[1706/2560vw] lg:max-h-screen  relative overflow-hidden bg-[#cfd7d6]`}
+      >
+        <img src={bg} alt="bg" className="hidden md:block overflow-hidden md:h-full lg:h-auto opacity-100" />
+        <img src={g7} alt="bg" className="block md:hidden w-full h-full " />
+        <div className="w-full h-full z-10 absolute top-0 left-0 flex flex-col items-center justify-end text-white md:text-gray-500 space-y-6 lg:space-y-8 p-4 pb-16 md:p-10 md:pb-24 lg:p-24 drop-shadow-2xl title">
+          <p className="text-[36px] md:text-[64px] text-center">
+          Việt Hoàn & Ngọc Quỳnh
           </p>
           <p className="text-[20px] md:text-[22px] uppercase !font-extrabold">
-            Lễ Vu Quy
+            Lễ Thành Hôn
           </p>
           <div className=" flex flex-col items-center">
             <p className="text-[20px] md:text-[22px] uppercase">
               Chủ nhật, 22.10.2023 11:00:00
             </p>
-            <p className="text-[16px]">Đông Hợp, Đông Hưng, Thái Bình</p>
+            <p className="text-[20px] md:text-[22px]">
+              Đông Hợp, Đông Hưng, Thái Bình
+            </p>
           </div>
         </div>
       </section>
@@ -178,7 +182,13 @@ function App() {
           <div className="flex flex-col-reverse items-center md:items-start md:flex-row gap-4">
             <div className="flex-1 lg:flex-none lg:w-[43%] p-10 border border-red-300 relative space-y-4">
               <div className="w-full h-full absolute border border-red-300 rotate-3 top-0 left-0" />
-              <p className="text-[#f67e7d] text-[28px]">Bữa cơm thân mật</p>
+              <p className="text-[#f67e7d] text-[28px]">
+                <span className=" whitespace-nowrap">Bữa cơm thân mật</span>{" "}
+                &nbsp;
+                <span className="text-[#f67e7d] text-[18px] whitespace-nowrap">
+                  (8h - 22/10/2023)
+                </span>
+              </p>
 
               <p>
                 <span className="text-[#f67e7d] italic">Nhà trai</span> - Thôn
@@ -201,7 +211,12 @@ function App() {
             </div>
             <div className="flex-1 lg:w-[43%] p-10 border border-red-300 relative">
               <div className="w-full h-full absolute border border-red-300 rotate-3 top-0 left-0" />
-              <p className=" text-[#f67e7d] text-[28px]">Lễ thành hôn</p>
+              <p className=" text-[#f67e7d] text-[28px]">
+                Lễ thành hôn &nbsp;
+                <span className="text-[#f67e7d] text-[18px] whitespace-nowrap">
+                  (11h - 22/10/2023)
+                </span>
+              </p>
               <p>
                 Thôn Phong Lôi Đông, xã Đông Hợp, huyện Đông Hưng, tỉnh Thái
                 Bình
@@ -286,10 +301,10 @@ function App() {
             Địa điểm
           </p>
           <div className="flex gap-10 flex-col lg:flex-row">
-            <div  className="flex flex-col gap-4 justify-center items-center w-full lg:w-1/2">
+            <div className="flex flex-col gap-4 justify-center items-center w-full lg:w-1/2">
               <p>Cô dâu</p>
               <iframe
-               title="mapdq"
+                title="mapdq"
                 src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3736.8251045637103!2d106.3500405752426!3d20.513394681010077!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjDCsDMwJzQ4LjIiTiAxMDbCsDIxJzA5LjQiRQ!5e0!3m2!1sen!2s!4v1696068787581!5m2!1sen!2s"
                 width="100%"
                 height="450"
@@ -302,22 +317,21 @@ function App() {
             <div className="flex flex-col gap-4 justify-center items-center w-full lg:w-1/2 ">
               <p>Chú rể</p>
               <iframe
-              title="mapdh"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3736.1071023040527!2d106.35540247465954!3d20.542798304293395!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135f02d583cb36f%3A0x72dba66dc029c579!2zQuG6v24geGUga2jDoWNoIMSQw7RuZyBIxrBuZw!5e0!3m2!1svi!2s!4v1695294470187!5m2!1svi!2s"
-              width="100%"
-              height="450"
-              style={{ border: 0 }}
-              allowfullscreen=""
-              loading="lazy"
-              referrerpolicy="no-referrer-when-downgrade"
-            ></iframe>
+                title="mapdh"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3736.1071023040527!2d106.35540247465954!3d20.542798304293395!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135f02d583cb36f%3A0x72dba66dc029c579!2zQuG6v24geGUga2jDoWNoIMSQw7RuZyBIxrBuZw!5e0!3m2!1svi!2s!4v1695294470187!5m2!1svi!2s"
+                width="100%"
+                height="450"
+                style={{ border: 0 }}
+                allowfullscreen=""
+                loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"
+              ></iframe>
             </div>
-           
           </div>
         </section>
 
-        <div className="capitalize title text-white w-full text-center p-4 lg:p-16 text-[42px] lg:text-[64px]">
-          {/* thank you! */}
+        <div className="capitalize title text-[#f67e7d] w-full text-center p-4 lg:p-16 text-[52px] lg:text-[74px] title my-6">
+          thank you!
         </div>
       </div>
       <button onclick="topFunction()" id="myBtn" title="Go to top">
